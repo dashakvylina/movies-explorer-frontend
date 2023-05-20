d5e8982d9da3504804132ce2ea40ea8cc0a3abd6
Daria Kovylina <dashakvylina60@yandex.ru>
2023-05-20 18:08:06 +0300

    fix

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,25 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
-import api from "../../utils/MainApi";
 
 const ProtectedRoute = ({ component: Component, loggedIn, path, ...props }) => {
-  const [logged, setLogged] = useState(loggedIn);
   const currentUser = React.useContext(CurrentUserContext);
-  console.log({ path, loggedIn, logged, currentUser, props });
-
-  // useEffect(() => {
-  //   api
-  //     .fetchUser()
-  //     .then((res) => {
-  //       if (res.email) {
-  //         console.log(res);
-
-  //         setLogged(true);
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
 
   return (
     !!currentUser && (
